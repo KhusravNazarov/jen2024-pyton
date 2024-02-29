919ef273b88468f76adcbd76b1d720aa7a4a83f9
Khusrav <onlyyou7400@gmail.com>
2024-02-29 03:14:28 +0000

    session2

---
+++ classwork/session2/session2.py
@@ -0,0 +1,37 @@
+# & -> Logical AND
+# | -> Logical Or
+# ~ -> Logical NOT
+
+# print(bin(2))
+# print(bin(2 << 3))
+# 
+
+
+#inp = int(print("write your number"))
+# if inp % 24 == 0:
+#     print("its even")
+# else:
+#     print("it's odd")
+    
+    # for var_name in sequence:
+    #     code....
+
+
+
+#for num in range(10): #range does not accept strings
+     #print(num) 
+
+num = int(input("Please give the number: "))
+for letter in range(num):
+    print (letter)   
+
+break, pass, continue 
+for else
+while else
+
+
+
+
+
+    
+
