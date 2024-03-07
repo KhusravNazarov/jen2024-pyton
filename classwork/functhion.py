45742cef4612c0971998017cbb4a4ca1ad76fb1f
Khusrav <onlyyou7400@gmail.com>
2024-03-07 03:08:29 +0000

    session5

---
+++ classwork/functhion.py
@@ -0,0 +1,58 @@
+# def checkEven(num):
+#     if num % 2 == 0:
+#         #always us retourn to get the data type
+#         return "This is even"
+#     else :
+#         return "This is odd"
+# even = 3 
+# print(checkEven(even))
+# print(checkEven(90))
+    
+    
+    
+    
+    
+# def converter(inp):
+#     f = (inp * 1.8) + 32
+#     print(f)
+# inp = int(input("Please provide your degree: "))
+# print(converter(inp))
+
+# def oddlist():
+#     return 
+#     inp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#     odd_list = []
+#     for num in inp:
+#         if num != 0:
+#            odd_list.append(num) 
+#     return num
+# print (returnn(odd_list))
+
+
+# def finder(list, char):
+#     word_lst = []
+#     for word in list:
+#         if char in word:
+#             word_lst.append(word)
+#     return word_lst
+# fruits = ["Banana", "Anor", "apple", "cherry"]
+# print(finder(fruits, "a"))
+
+# def adder(inp1, inp2):
+#     return inp1 * inp2
+# in1 = 10
+# in2 = 10
+# print(adder(in1, in2))
+
+# def check(word, char):
+#     return word, char
+# print()
+
+   
+
+
+
+    
+    
+
+    
